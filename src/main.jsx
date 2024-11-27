5e80f54a6da06d77eb65e7778ab6ab1c8390c4bc
Daksh Choudhary <ch.daksh01@gmail.com>
2024-11-27 16:12:01 +0530

    created context apis

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AuthContext from './Contexts/AuthContext.jsx'
+import TaskContext from './Contexts/TaskContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthContext>
+      <TaskContext>
+        <App />
+      </TaskContext>
+    </AuthContext>
   </StrictMode>,
 )
