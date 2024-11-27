b29dd7b207c8f831ff17470f99cad5026c18f195
Daksh Choudhary <ch.daksh01@gmail.com>
2024-11-27 22:02:18 +0530

    combined and made a single context api

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AuthContext from './Contexts/AuthContext.jsx'
-import TaskContext from './Contexts/TaskContext.jsx'
+import AuthProvider from './Contexts/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthContext>
-      <TaskContext>
+      <AuthProvider>
         <App />
-      </TaskContext>
-    </AuthContext>
+      </AuthProvider>
+
   </StrictMode>,
 )
